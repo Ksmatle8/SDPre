e70e70f6780fcb0f2194a73a3f9f2107dd13ff1d
hatle$ <kayla.hatle@yahoo.com>
2019-05-10 20:41:58 -0600

    finished

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,17 @@
+public class Objective2Lab2 {
+  public static void main(String[] arg) {
+    String one, two, both;
+    int years;
+    String state;
+
+    one = "Kayla";
+    two = " Hatle";
+    both = one + two;
+    years = 32;
+    state = "Colorado";
+
+    System.out.print( " Name: " + both );
+    System.out.print( " Age: " + years );
+    System.out.print( " State: " + state );
+    }
+}
