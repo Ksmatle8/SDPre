67f47e14f5aae9402606513066218ce31134e439
hatle$ <kayla.hatle@yahoo.com>
2019-05-10 20:36:33 -0600

    finished

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,11 @@
+public class Objective2Lab5 {
+  public static void main(String[] args) {
+    double side1, side2, hypo;
+    side1 = 10;
+    side2 = 8;
+    hypo = (side1 * side1) + (side2 * side2);
+    double num = Math.sqrt(hypo);
+    //double total = hypo;
+    System.out.println("total:" + num);
+  }
+}
